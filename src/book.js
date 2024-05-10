33b7b7131afcad8cee28bb99bfea347c02af64d6
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-05-10 17:47:27 -0600

    book.js finished

--- src/book.js
+++ src/book.js
@@ -1,12 +1,49 @@
-function createTitle() {
-  
+function createTitle(tital) {
+  return `The ${tital}`
 }
+function buildMainCharacter (name,age,pronouns){
+var mainCharacter = {
+  name:name,
+  age:age,
+  pronouns:pronouns
+}
+return mainCharacter
+}
+function saveReview(info, review){
+  var hasItem = false
+  for (var i = 0;i < review.length; ++i){
+    if (info === review[i]) {
+      hasItem = true
+    }
+  }
+  if (!hasItem) {
+    review.push(info)
+  }
+return review
+}
+function calculatePageCount (bookTitle){
+  return bookTitle.length * 20
 
+}
+function writeBook (title,mainCharacter,genre) {
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+
+  }
+  return book
+}
+function editBook (book){
+  book.pageCount = book.pageCount * .75
+  return book
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+   buildMainCharacter,
+  saveReview,
+   calculatePageCount,
+   writeBook,
+   editBook
 }
--- src/warm-up.js
+++ src/warm-up.js
@@ -10,29 +10,35 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
+ var title = "Harry Potter";
+ var pages = 150 ;
 
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
-
+pages = 150 / 25
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie =
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var sequel = "Harry poter and the chamber of secrets"
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
+var isFavorite = true
+if (title === "Harry Potter" ){
 
+    isFavorite = true
+} else {
+    isFavorite = false
+}
 
 
 
